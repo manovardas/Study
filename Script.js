61ff936209989968b8bccc92e065d33d4e559658
Giedrelis <giedriusb@glasslt.com>
2025-09-22 09:37:09 +0300

    Function test

--- Script.js
+++ Script.js
@@ -1,3 +1,11 @@
 // JavaScript source code
 
 
+console.log("JS failas veikia!");
+
+const mygtukas = document.getElementById("mygtukas");
+const rezultatas = document.getElementById("rezultatas");
+
+mygtukas.addEventListener("click", () => {
+    rezultatas.innerText = "Mygtukas paspaustas!";
+});
